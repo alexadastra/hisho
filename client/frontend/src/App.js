d9ec84127f9c6555179afc518de8c8d319f71fb9
alexadastra <semenchenkoaleksey92@gmail.com>
2023-05-24 13:26:21 +0300

    [dev] add test client to try some stuff

---
+++ client/frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Orders from './components/orders.components';
+
+function App() {
+  return (
+    <div>
+      <Orders />
+    </div>
+  );
+}
+
+export default App
